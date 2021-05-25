679f8d3ba2e8c547ea3b7d580b0ea5c9d037e579
Ranjini <ranj.jini@gmail.com>
2021-05-25 12:24:26 +0100

    Tests to validate creation of Plateau

---
+++ marsRoverChallenge/createPlateau.js
@@ -0,0 +1,20 @@
+//function to create plateau
+function createPlateau(x, y){
+  let plateau = [x,y];
+  //check if plateau co-ordinates are valid
+  if(x === 0 || y === 0){
+    return "Error: Co-ordinate cannot be zero";
+  }
+  if(x < 0 || y < 0){
+    return "Error: Co-ordinate cannot be negative";
+  }
+  if(isNaN(x) || isNaN(y) || x === null || y === null){
+    return "Error: Plateau co-ordinates cannot contain characters";
+  }
+  return plateau;
+}
+
+module.exports = {
+  createPlateau
+}
+
