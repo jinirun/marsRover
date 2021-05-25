64a36426c67664b4a4bdd79d8f3781d962daad8d
Ranjini <ranj.jini@gmail.com>
2021-05-25 14:04:51 +0100

    Added tests to check for Plateau co-ordinates

--- marsRoverChallenge/createRover.js
+++ marsRoverChallenge/createRover.js
@@ -4,11 +4,24 @@ function createRover(plateau, x, y, direction){
   let hasNumber = /\d/;
   let validDirections    = ['N', 'S', 'W', 'E'];
 
+  if(plateau[0] === 0 || plateau[1] === 0){
+    return "Error: Plateau co-ordinate cannot be zero";
+  }
+  if(plateau[0] < 0 || plateau[1] < 0){
+    return "Error: Plateau co-ordinate cannot be negative";
+  }
+  if(isNaN(plateau[0]) || isNaN(plateau[1])){
+    return "Error: Plateau co-ordinates cannot contain characters";
+  }
+
   if(isNaN(x) || isNaN(y)){
     return "Error: Rover co-ordinates cannot contain characters";
   }
   if(x > rover.plateauCoordinates[0] || y > rover.plateauCoordinates[1]){
     return "Error: Rover co-ordinates have to be less than plateau co-ordinates";
+  }
+  if(x < 0 || y < 0){
+    return "Error: Rover co-ordinates cannot be negative";
   }
 
   if(hasNumber.test(direction)){
--- marsRoverTest/createRover.test.js
+++ marsRoverTest/createRover.test.js
@@ -3,13 +3,32 @@ const{
 } = require("../marsRoverChallenge/createRover");
 
 describe("createRover", () => {
-  test("Returns an error if a character or string is passed as the plateau dimension", () => {
+  test("Returns an error if 0 is passed as the plateau co-ordinate", () => {
+    expect(createRover([0,5], 4,5,'N')).toBe("Error: Plateau co-ordinate cannot be zero");
+    expect(createRover([4,0], 4,5,'N')).toBe("Error: Plateau co-ordinate cannot be zero");
+    expect(createRover([0,0], 4,5,'N')).toBe("Error: Plateau co-ordinate cannot be zero");
+  })
+  test("Returns an error if negative numbers are entered as plateau co-ordinates", () => {
+    expect(createRover([-1,4],  4,5,'N')).toBe("Error: Plateau co-ordinate cannot be negative");
+    expect(createRover([6,-3],  4,5,'N')).toBe("Error: Plateau co-ordinate cannot be negative");
+    expect(createRover([-6,-3], 4,5,'N')).toBe("Error: Plateau co-ordinate cannot be negative");
+  })
+  test("Returns an error if characters are passed as plateau co-ordinates", () => {
+    expect(createRover(['y',5], 3,5,'E')).toBe("Error: Plateau co-ordinates cannot contain characters");
+    expect(createRover([9,'l'], 3,5,'W')).toBe("Error: Plateau co-ordinates cannot contain characters");
+    expect(createRover(['y','t'],4,5,'N')).toBe("Error: Plateau co-ordinates cannot contain characters");
+  })
+  test("Returns an error if a character or string is passed as the rover dimension", () => {
     expect(createRover([5,5], 'g',5,'N')).toBe("Error: Rover co-ordinates cannot contain characters");
     expect(createRover([5,5],5,'h','E')).toBe("Error: Rover co-ordinates cannot contain characters");
   })
   test("Returns an error if co-ordinates are greater than the plateau dimensions", () => {
     expect(createRover([5,5],6,5,'N')).toBe("Error: Rover co-ordinates have to be less than plateau co-ordinates");
     expect(createRover([5,5],5,6,'E')).toBe("Error: Rover co-ordinates have to be less than plateau co-ordinates");
+  })
+  test("Returns an error if co-ordinates are negative", () => {
+    expect(createRover([5,5],-6,5,'N')).toBe("Error: Rover co-ordinates cannot be negative");
+    expect(createRover([5,5],5,-6,'E')).toBe("Error: Rover co-ordinates cannot be negative");
   })
 
   test("Returns an error message if the direction passed is empty", () => {
