794839d4352f0b24b1b6acf4b0e3a56a18e72731
Ranjini <ranj.jini@gmail.com>
2021-05-26 12:02:30 +0100

    Added a new function to check for possible collision between rovers

---
+++ marsRoverChallenge/checkCollision.js
@@ -0,0 +1,27 @@
+const{
+  positioning
+} = require ("../marsRoverChallenge/positioning");
+
+//function to check for collision
+function checkCollision(rover, instructions, x, y){
+  let roverTemp = JSON.parse(JSON.stringify(rover));
+  let errorMsg = "";
+
+  for(let i=0; i<instructions.length; i++) {
+    switch (instructions[i]) {
+      case 'L': roverTemp.direction = turnLeft(roverTemp.direction);
+        break;
+      case 'R': roverTemp.direction = turnRight(roverTemp.direction);
+        break;
+      case 'M': [roverTemp.xCoordinate, roverTemp.yCoordinate] = move(roverTemp);
+        if(roverTemp.xCoordinate === x && roverTemp.yCoordinate === y) {
+          errorMsg = "Error: Possible collision at co-ordinates " + x + " " + y;
+        }
+    }
+  }
+  return errorMsg;
+}
+
+module.exports = {
+  checkCollision
+}
---
+++ marsRoverTest/checkCollision.test.js
@@ -0,0 +1,12 @@
+const{
+  checkCollision
+} = require ("../marsRoverChallenge/checkCollision");
+
+describe("checkCollision", () => {
+  test("Returns an error message when a possible collision is detected", () => {
+    expect(checkCollision({plateauCoordinates : [5,5], xCoordinate : 2, yCoordinate: 2, direction: 'N'}, 'MMRMRMRM', 3, 3)).toBe("Error: Possible collision at co-ordinates 3 3");
+  })
+  test("Returns an empty string when there is no possible collision", () => {
+    expect(checkCollision({plateauCoordinates : [5,5], xCoordinate : 1, yCoordinate: 2, direction: 'N'}, 'LMLMLMLMM', 0, 0)).toBe("");
+  })
+})
