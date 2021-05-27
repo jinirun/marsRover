3d2792ed96434dea59279920cea4c0681ce5116a
Ranjini <ranj.jini@gmail.com>
2021-05-27 11:23:23 +0100

    Modified to throw and test exceptions instead of returning error messages

--- marsRoverTest/createPlateau.test.js
+++ marsRoverTest/createPlateau.test.js
@@ -2,29 +2,39 @@ const{
     createPlateau
 } = require("../marsRoverChallenge/createPlateau");
 
+//implemented tests to log errors (expect-toThrow)
 describe("createPlateau", () => {
   test("Returns an error if 0 is passed as the co-ordinate", () => {
-    // expect(createPlateau(0, 5)).toBe("Error: Co-ordinate cannot be zero");
-    // expect(createPlateau(4, 0)).toBe("Error: Co-ordinate cannot be zero");
-    // expect(createPlateau(0, 0)).toBe("Error: Co-ordinate cannot be zero");
     expect(() => {
       createPlateau(0, 5);
     }).toThrow("Error: Co-ordinate cannot be zero");
+    expect(() => {
+      createPlateau(4, 0);
+    }).toThrow("Error: Co-ordinate cannot be zero");
+    expect(() => {
+      createPlateau(0, 0);
+    }).toThrow("Error: Co-ordinate cannot be zero");
   })
   test("Returns an error if negative numbers are entered as plateau co-ordinates", () => {
-    // expect(createPlateau(-1, 4)).toBe("Error: Co-ordinate cannot be negative");
-    // expect(createPlateau(6, -3)).toBe("Error: Co-ordinate cannot be negative");
-    // expect(createPlateau(-6, -3)).toBe("Error: Co-ordinate cannot be negative");
     expect(() => {
       createPlateau(-1, 4);
     }).toThrow("Error: Co-ordinate cannot be negative");
+    expect(() => {
+      createPlateau(6, -3);
+    }).toThrow("Error: Co-ordinate cannot be negative");
+    expect(() => {
+      createPlateau(-6, -3);
+    }).toThrow("Error: Co-ordinate cannot be negative");
   })
   test("Returns an error if characters are passed as plateau co-ordinates", () => {
-    // expect(createPlateau('y', 5)).toBe("Error: Plateau co-ordinates cannot contain characters");
-    // expect(createPlateau(9, 'l')).toBe("Error: Plateau co-ordinates cannot contain characters");
-    // expect(createPlateau('y', 't')).toBe("Error: Plateau co-ordinates cannot contain characters");
     expect(() => {
       createPlateau('y', 5);
+    }).toThrow("Error: Plateau co-ordinates cannot contain characters");
+    expect(() => {
+      createPlateau(9, 'l');
+    }).toThrow("Error: Plateau co-ordinates cannot contain characters");
+    expect(() => {
+      createPlateau('y', 't');
     }).toThrow("Error: Plateau co-ordinates cannot contain characters");
   })
   test("Returns co-ordinates if positive numbers are entered", () => {
