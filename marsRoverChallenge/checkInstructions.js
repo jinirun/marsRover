1a08eaccfed7920f5daaaaa7167165eb74e2a2b6
Ranjini <ranj.jini@gmail.com>
2021-05-27 12:00:50 +0100

    Modified to throw and test exceptions instead of returning error messages

--- marsRoverChallenge/checkInstructions.js
+++ marsRoverChallenge/checkInstructions.js
@@ -1,29 +1,30 @@
 //function to check for valid instructions
-function checkInstructions(instructions){
-  let validInstructions  = ['L', 'R', 'M'];
+function checkInstructions(instructions) {
+  let validInstructions = ["L", "R", "M"];
   let hasNumber = /\d/;
   let format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
   let str = "";
 
-  if(instructions === ''){
-    return "Error: Instruction cannot be empty";
+  if (instructions === "") {
+    throw new Error("Error: Instruction cannot be empty");
   }
-  if(hasNumber.test(instructions)){
-    return "Error: Instruction cannot contain a number";
+  if (hasNumber.test(instructions)) {
+    throw new Error("Error: Instruction cannot contain a number");
   }
-  if(format.test(instructions)){
-    return "Error: Instruction cannot contain special characters";
+  if (format.test(instructions)) {
+    throw new Error("Error: Instruction cannot contain special characters");
   }
-
   str = instructions.split("");
-  for(let i=0; i<str.length; i++){
-    if (!validInstructions.includes(str[i])){
-      return "Error: Instructions should contain letters L, R and M only";
+  for (let i = 0; i < str.length; i++) {
+    if (!validInstructions.includes(str[i])) {
+      throw new Error(
+        "Error: Instructions should contain letters L, R and M only"
+      );
     }
   }
   return instructions;
 }
 
 module.exports = {
-  checkInstructions
-}
+  checkInstructions,
+};
--- marsRoverTest/checkInstructions.test.js
+++ marsRoverTest/checkInstructions.test.js
@@ -1,27 +1,44 @@
-const{
-  checkInstructions
+const {
+  checkInstructions,
 } = require("../marsRoverChallenge/checkInstructions");
 
 describe("checkInstructions", () => {
   test("Returns an error message if the instruction passed is empty", () => {
-    expect(checkInstructions('')).toBe("Error: Instruction cannot be empty");
-  })
+    expect(() => {
+      checkInstructions("");
+    }).toThrow("Error: Instruction cannot be empty");
+  });
   test("Returns an error message if the instruction contains letters other than L, R or M", () => {
-    expect(checkInstructions('G')).toBe("Error: Instructions should contain letters L, R and M only");
-  })
+    expect(() => {
+      checkInstructions("G");
+    }).toThrow("Error: Instructions should contain letters L, R and M only");
+  });
   test("Returns an error if the instructions contain letters other than L, R or M in a longer list of instructions", () => {
-    expect(checkInstructions('LMLMLMGLM')).toBe("Error: Instructions should contain letters L, R and M only");
-  })
+    expect(() => {
+      checkInstructions("LMLMLMGLM");
+    }).toThrow("Error: Instructions should contain letters L, R and M only");
+  });
   test("Returns an error if a number is passed as an instruction", () => {
-    expect(checkInstructions(3)).toBe("Error: Instruction cannot contain a number");
-  })
+    expect(() => {
+      checkInstructions(3);
+    }).toThrow("Error: Instruction cannot contain a number");
+  });
   test("Returns an error if a number is passed in a longer list of instructions", () => {
-    expect(checkInstructions('LMMRM3L')).toBe("Error: Instruction cannot contain a number");
-  })
+    expect(() => {
+      checkInstructions("LMMRM3L");
+    }).toThrow("Error: Instruction cannot contain a number");
+  });
   test("Returns an error if a special character is passed as an instruction", () => {
-    expect(checkInstructions('!')).toBe("Error: Instruction cannot contain special characters");
-  })
+    expect(() => {
+      checkInstructions("!");
+    }).toThrow("Error: Instruction cannot contain special characters");
+  });
   test("Returns an error if a special character is passed in a longer list of instructions", () => {
-    expect(checkInstructions('LMMRM&L')).toBe("Error: Instruction cannot contain special characters");
-  })
-})
+    expect(() => {
+      checkInstructions("LMMRM&L");
+    }).toThrow("Error: Instruction cannot contain special characters");
+  });
+  test("Returns the instructions if all instructions are valid", () => {
+    expect(checkInstructions("LMLMLMLMM")).toBe("LMLMLMLMM");
+  });
+});
