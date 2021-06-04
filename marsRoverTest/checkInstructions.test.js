43e2b3bdb6a183b53a345f36aecb925948d301c4
Ranjini <ranj.jini@gmail.com>
2021-06-04 22:28:55 +0100

    Modified to replace test with test.each for repeating tests

--- marsRoverTest/checkInstructions.test.js
+++ marsRoverTest/checkInstructions.test.js
@@ -9,39 +9,24 @@ describe("checkInstructions", () => {
     }).toThrow("Error: Instruction cannot be empty");
   });
 
-  test("Returns an error message if the instruction contains letters other than L, R or M", () => {
+  test.each([["G"], ["LMLMLMGLM"]])
+    ("Returns an error message if the instruction contains letters other than L, R or M", (str) => {
     expect(() => {
-      checkInstructions("G");
+      checkInstructions(str);
     }).toThrow("Error: Instructions should contain letters L, R and M only");
   });
 
-  test("Returns an error if the instructions contain letters other than L, R or M in a longer list of instructions", () => {
+  test.each([[3], ["LMMRM3L"]])
+    ("Returns an error if a number is passed as an instruction", (str) => {
     expect(() => {
-      checkInstructions("LMLMLMGLM");
-    }).toThrow("Error: Instructions should contain letters L, R and M only");
-  });
-
-  test("Returns an error if a number is passed as an instruction", () => {
-    expect(() => {
-      checkInstructions(3);
+      checkInstructions(str);
     }).toThrow("Error: Instruction cannot contain a number");
   });
 
-  test("Returns an error if a number is passed in a longer list of instructions", () => {
+  it.each([["!"], ["LMMRM&L"]])
+    ("Returns an error if a special character is passed as an instruction", (str) => {
     expect(() => {
-      checkInstructions("LMMRM3L");
-    }).toThrow("Error: Instruction cannot contain a number");
-  });
-
-  test("Returns an error if a special character is passed as an instruction", () => {
-    expect(() => {
-      checkInstructions("!");
-    }).toThrow("Error: Instruction cannot contain special characters");
-  });
-
-  test("Returns an error if a special character is passed in a longer list of instructions", () => {
-    expect(() => {
-      checkInstructions("LMMRM&L");
+      checkInstructions(str);
     }).toThrow("Error: Instruction cannot contain special characters");
   });
   
